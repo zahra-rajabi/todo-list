e7bcd7c3f875607cae606b19f72f2c1c68042292
Zahra Rajabi <zahra.80.rajabi@gmail.com>
2024-04-22 16:15:19 +0330

    get data from local storage

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
 let input = document.querySelector(".task-write__container").firstElementChild;
 let button = document.querySelector(".task-write__container").lastElementChild;
 let list = document.querySelector(".task-list__items");
+let tasks;
+
+if (!localStorage.getItem("todo")) {
+  tasks = [];
+} else {
+  getTasks();
+}
 
 button.addEventListener("click", function () {
   if (input.value !== "") {
     list.innerHTML += `
-<li class="task-list__items-item">
-  <ion-icon name="checkmark-outline" class='icon icon-mark'></ion-icon>
-
-  ${input.value}
- 
-  <ion-icon name="trash-outline" class='icon icon-trash'></ion-icon>
+<li class="task-list__items-item"><ion-icon name="checkmark-outline" class='icon icon-mark'></ion-icon>${input.value}<ion-icon name="trash-outline" class='icon icon-trash'></ion-icon>
 </li>`;
+    saveTasks(input.value);
     input.value = "";
   }
 });
 
 list.addEventListener("click", function (event) {
   if (event.target.classList.contains("icon-trash")) {
-    event.target.parentElement.style.display = "none";
+    let target = event.target.parentElement;
+    let targetText = target.textContent.replace(" ", "");
+    let indexTarget = tasks.indexOf("targetText");
+
+    target.style.display = "none";
   }
   if (event.target.nodeName == "LI") {
     event.target.classList.toggle("task-list__items-item--active");
@@ -28,3 +35,19 @@ list.addEventListener("click", function (event) {
     );
   }
 });
+function saveTasks(text) {
+  tasks.push(text);
+  localStorage.setItem("todo", tasks);
+}
+function getTasks() {
+  let todo = localStorage.getItem("todo");
+  tasks = todo.split(",");
+  return tasks;
+}
+let previousTasks = getTasks();
+
+for (let element of previousTasks) {
+  list.innerHTML += `
+<li class="task-list__items-item"><ion-icon name="checkmark-outline" class='icon icon-mark'></ion-icon>${element}<ion-icon name="trash-outline" class='icon icon-trash'></ion-icon>
+</li>`;
+}
